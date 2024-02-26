d637618f0050ecd49077c026c601c674f03b55f4
unknown <namitlodh>
2024-02-26 19:09:02 +0530

    [Namit] Add : User login with email and password

--- FunDoNotes/Controllers/UserController.cs
+++ FunDoNotes/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using Manager_Layer.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Repository_layer.Entity;
+using System;
 
 namespace FunDoNotes.Controllers
 {
@@ -21,14 +22,35 @@ namespace FunDoNotes.Controllers
         [Route("Reg")]
         public  ActionResult Register(RegisterModel model)
         {
-            var repsonse = userManager.UserRegisteration(model);
-            if(repsonse != null)
+            try
             {
-                return Ok(new ResModel<User> { Success = true, Message = "register successfull", Data = repsonse });
+                var repsonse = userManager.UserRegisteration(model);
+                if (repsonse != null)
+                {
+                    return Ok(new ResModel<User> { Success = true, Message = "register successfull", Data = repsonse });
+                }
+                else
+                {
+                    return BadRequest(new ResModel<User> { Success = false, Message = "Resgister failed", Data = repsonse });
+                }
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(new ResModel<User> { Success = false, Message = ex.Message, Data = null });
+            }
+        }
+        [HttpPost]
+        [Route("Log")]
+        public ActionResult Login(LoginModel model)
+        {
+            string response = userManager.UserLogin(model);
+            if (response != null)
+            {
+                return Ok(new ResModel<string> { Success=true, Message="Login successfull", Data=response});
             }
             else
             {
-                return BadRequest(new ResModel<User> { Success = false, Message = "Resgister failed", Data= repsonse });
+                return BadRequest(new ResModel<string> { Success = false, Message = "Login failed", Data = response });
             }
         }
     }
--- FunDoNotes/Startup.cs
+++ FunDoNotes/Startup.cs
@@ -35,6 +35,7 @@ namespace FunDoNotes
             services.AddTransient<IUserRepository, UserRepository>();
             services.AddTransient<IUserManager, UserManager>();
             services.AddControllers();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,7 +45,13 @@ namespace FunDoNotes
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseSwagger();
 
+            // This middleware serves the Swagger documentation UI
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Employee API V1");
+            });
             app.UseHttpsRedirection();
 
             app.UseRouting();
